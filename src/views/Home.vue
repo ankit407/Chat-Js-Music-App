e673632d7a338dedae6fca833111f2c2f8057740
Ankit Meena <106733643+ankit407@users.noreply.github.com>
2023-02-13 11:50:38 +0530

    Create Music App In Vue js Using Chart Js

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app :style="{background: $vuetify.theme.themes.dark.background}">
+    <SideBar />
+    <RightSideBar />
+    <Dashboard />
+  </v-app>
+</template>
+
+<script>
+  import SideBar from '../components/SideBar'
+  import RightSideBar from '../components/RightSideBar'
+  import Dashboard from '../components/Dashboard'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      SideBar,
+      RightSideBar,
+      Dashboard
+      
+    },
+  }
+</script>
